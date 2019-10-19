f0a05c6951a6a108812b1301cff991c395071559
ajacker <ajacker@foxmail.com>
2019-10-19 23:09:40 +0800

    完成：二维数组中的查找

--- src/question_01.java
+++ src/question_01.java
@@ -5,6 +5,31 @@
 public class question_01 {
 
     public static void main(String[] args) {
-
+        int[][] array = new int[][]{
+                {1,2,8,9},
+                {2,4,9,12},
+                {4,7,10,13},
+                {6,8,11,15}
+        };
+        int target = 3;
+        System.out.println(Find(target, array));
     }
+    static boolean Find(int target, int[][] array) {
+        //从数组右上角开始查找，因为特点是左侧都比它小，下侧都比他大
+        int i = 0;
+        int j = array[0].length - 1;
+        //保证i，j在合法的范围
+        while (i>=0 && i < array.length && j>=0 && j < array[i].length) {
+            int cur = array[i][j];
+            if (target == cur) {
+                return true;
+            } else if (target > cur) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+
 }
