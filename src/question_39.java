c9d4dc8fe41f96000ff28ea3af06c9bcf17d26f0
ajacker <ajacker@foxmail.com>
2019-10-24 22:25:11 +0800

    完成：平衡二叉树

---
+++ src/question_39.java
@@ -0,0 +1,72 @@
+/**
+ * @author ajacker
+ * @date 2019/10/24 22:15
+ * @see <a href="平衡二叉树">https://www.nowcoder.com/practice/8b3b95850edb4115918ecebdf1b4d222</a>
+ */
+public class question_39 {
+    /**
+     * flag标志是否为avl
+     */
+    static boolean flag = true;
+
+    public static void main(String[] args) {
+        TreeNode t0 = new TreeNode(8);
+        TreeNode t1 = new TreeNode(6);
+        TreeNode t2 = new TreeNode(10);
+        TreeNode t3 = new TreeNode(5);
+        TreeNode t4 = new TreeNode(7);
+        TreeNode t5 = new TreeNode(9);
+        TreeNode t6 = new TreeNode(1);
+        t0.left = t1;
+        t0.right = t2;
+        t1.left = t3;
+        t1.right = t4;
+        t2.left = t5;
+        t2.right = t6;
+        System.out.println(IsBalanced_Solution(t0));
+    }
+
+    /**
+     * 判断是否是平衡二叉树
+     * 左右子树的深度之差不会超过1
+     *
+     * @param root
+     * @return
+     */
+    static boolean IsBalanced_Solution(TreeNode root) {
+        TreeDepth(root);
+        return flag;
+    }
+
+    /**
+     * 递归求树深度并判断是否是平衡二叉树
+     * 左右子树的深度之差不会超过1
+     *
+     * @param root 树根节点
+     * @return 深度
+     */
+    static int TreeDepth(TreeNode root) {
+        //如果知道不是avl就尽早返回
+        if (!flag || root == null) {
+            return 0;
+        }
+        int leftDepth = TreeDepth(root.left);
+        int rightDepth = TreeDepth(root.right);
+        //左右子树的深度之差不会超过1
+        flag = Math.abs(leftDepth - rightDepth) <= 1;
+        //任意一棵树的深度为左子树和右子树深度最大值+1（本节点深度）
+        return Integer.max(leftDepth, rightDepth) + 1;
+    }
+
+    static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+}
